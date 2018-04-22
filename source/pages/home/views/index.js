45748f97f24a2ae623ced8662aa4ab9539cb6a03
enuobear <enuobear@gmail.com>
2018-04-22 23:09:24 +0800

    update

--- source/pages/home/views/index.js
+++ source/pages/home/views/index.js
@@ -1,5 +1,13 @@
 'use strict';
 Fm.controller('cIndex', function (
-    $scope
+    $scope,
+    $appueWidget
 ){
+    $appueWidget.ajaxRequest({
+        scope: $scope,
+        url: 'getAdmin',
+        success: function (res) {
+            console.log(res);
+        }
+    });
 });
